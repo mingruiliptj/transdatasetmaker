ddfdc98331569e25404158f1a30e227a98832fb1
lmr <xyshyniaphy@gmail.com>
2025-03-25 17:15:43 +0900

    Signed-off-by: lmr <xyshyniaphy@gmail.com>

--- replace_newlines.py
+++ replace_newlines.py
@@ -27,15 +27,3 @@ if __name__ == "__main__":
     filename = sys.argv[1]
     result = replace_newlines(filename)
     print(result)
-
-    # Test case
-    test_filename = "test_file.txt"
-    with open(test_filename, 'w', encoding='utf-8') as f:
-        f.write("This is a test.\nThis is a single newline.\n\nThis is a double newline.\nThis is another single newline.\n\n")
-
-    replace_newlines(test_filename)
-
-    with open(test_filename, 'r', encoding='utf-8') as f:
-        test_content = f.read()
-
-    print(f"Test result: {test_content}")
