ea4b7ebd1ca60bf55eadc15d9da610ab16853490
lmr <xyshyniaphy@gmail.com>
2025-03-25 17:57:01 +0900

    upd

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ huggingface-cli login
 
 pip install -r requirements
 
-python create_translation_dataset.py --chinese_text towest_cn.txt --english_text towest_en.txt  --to_simplified --push_to_hub --repo_name xyshyniaphy/cn2en_s --private False
+python create_translation_dataset.py --chinese_text towest_cn.txt --english_text towest_en.txt  --to_simplified --push_to_hub --repo_name xyshyniaphy/cn2en_s
